940c317c6d4504f1ca444f344705d75bebc96b65
Faisal-Akram <zjan45097@gmail.com>
2024-09-29 14:23:07 +0500

    user in null alert

--- java.js
+++ java.js
@@ -1,14 +1,17 @@
-var user =  document.getElementById("user-d.o.b");
-var years = document.getElementById('years');
-var months = document.getElementById('months');
-var days = document.getElementById('days');
+
 
 
 
 
 function calculate() {
-      years.textContent = user.value;
-      user.value = ''
+var inputDate =  document.getElementById("user-d.o.b").value;
+
+    if(!inputDate) {
+        alert("plese select your age ")
+        return;
+    }
+
 
 }
+
 
