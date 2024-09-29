1e43813e2953bf901fa577e7396b5931dccb2756
Faisal-Akram <zjan45097@gmail.com>
2024-09-29 10:14:56 +0500

    css is almost completed

---
+++ java.js
@@ -0,0 +1,16 @@
+// var user =  document.getElementById("user-d.o.b");
+// document.getElementById("calculate");
+// var years = document.getElementById('years');
+// var months = document.getElementById('months');
+// var days = document.getElementById('days');
+// var a = document.getElementById('a')
+
+// var input = document.getElementById('input');
+
+
+
+// function calculate() {
+//       a.textValue = input.textContent;
+
+// }
+
